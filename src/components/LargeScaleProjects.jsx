8d3ce17cb0e4ddb77b2383642c3706abc4981567
João Pedro da Silva <joao.pedro.da.silva@hotmail.com>
2024-02-15 22:05:57 -0300

    change width of large scale project iframe

--- src/components/LargeScaleProjects.jsx
+++ src/components/LargeScaleProjects.jsx
@@ -6,7 +6,7 @@ export const LargeScaleProjects = () => (
         <div className='h-full flex justify-center gap-10'>
             <iframe
                 title="financas-e-fundamentos-iframe"
-                className="hidden lg:flex w-full max-w-[610px] aspect-[4/5] rounded-lg"
+                className="hidden lg:flex w-full max-w-[1024px] rounded-lg"
                 src="https://financas-e-fundamentos.netlify.app/"
             />
 
@@ -17,13 +17,13 @@ export const LargeScaleProjects = () => (
                     <p className='block lg:hidden text-xs text-center pt-2 pb-6 text-yellow-400'>Please, note the iframe displaying this project will only appear at desktops. If you are using small screen devices, click the link below and visit the webpage itself.</p>
                 </div>
                 <p><a
-                        href="https://financas-e-fundamentos.netlify.app/"
-                        target="_blank" rel="noreferrer"
-                        className='underline cursor-pointer'
-                    >Finanças e Fundamentos website
-                    </a> is my largest project.</p>
+                    href="https://financas-e-fundamentos.netlify.app/"
+                    target="_blank" rel="noreferrer"
+                    className='underline cursor-pointer'
+                >Finanças e Fundamentos website
+                </a> is my largest project.</p>
                 <p>It's about financial education, the brazilian stocks market and some of the most important brazilian macroeconomics metrics.</p>
-                <p>I've built it using the PERN stack - Node.js and Express.js as backend, PostgreSQL as database, and React.js as frontend - I also used TailwindCss for styling and D3.js for data visualization. </p>
+                <p>I've built it using the PERN stack - Node.js and Express.js as backend, PostgreSQL as database, and React.js as frontend - I also used TailwindCss for styling and Charts.js for data visualization. </p>
                 <p>The plan for the future is to implement a user authentication method, maybe using passport.js. Hopefully, I will implement some quality assurance too, with automated tests and Typescript, for example.</p>
             </div>
         </div>
